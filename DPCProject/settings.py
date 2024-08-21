1ef66043adf8454ed6671fb801d876f48c9da247
unknown <ashishkr9214jmp@gmail.com>
2024-08-22 01:51:07 +0530

    first commit domain comparator app

--- DPCProject/settings.py
+++ DPCProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = os.environ.get("DEBUG")
+DEBUG = os.environ.get("DEBUG").lower() == "true"
 
 # ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
